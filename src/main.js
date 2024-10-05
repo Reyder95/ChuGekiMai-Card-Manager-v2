62a8424aa7754927c9194ed36018cdd69312ec1a
Reyder95 <khoutas98@gmail.com>
2024-10-04 22:50:10 -0400

    Fixed some tray issues, and second instance problems

--- src/main.js
+++ src/main.js
@@ -28,105 +28,116 @@ const path = __importStar(require("path"));
 const fs = __importStar(require("fs"));
 let mainWindow;
 let tray = null;
-const createWindow = () => {
-    tray = new electron_1.Tray(path.join(__dirname, "trayIcon.png"));
-    const contextMenu = electron_1.Menu.buildFromTemplate([
-        { label: 'Show Overlay', click: showOverlay },
-        { label: 'Quit', click: quitApp }
-    ]);
-    tray.setToolTip('My Background App');
-    tray.setContextMenu(contextMenu);
-    mainWindow = new electron_1.BrowserWindow({
-        width: 400,
-        height: 600,
-        resizable: false,
-        autoHideMenuBar: true,
-        opacity: 0.8,
-        webPreferences: {
-            nodeIntegration: false,
-            preload: path.join(__dirname, 'preload.js'),
-            contextIsolation: true, // Isolate context for security
+const lock = electron_1.app.requestSingleInstanceLock();
+if (!lock) {
+    electron_1.app.quit();
+}
+else {
+    const createWindow = () => {
+        if (!tray) {
+            tray = new electron_1.Tray(path.join(__dirname, "trayIcon.png"));
+            const contextMenu = electron_1.Menu.buildFromTemplate([
+                { label: 'Show Overlay', click: showOverlay },
+                { label: 'Quit', click: quitApp }
+            ]);
+            tray.setToolTip('My Background App');
+            tray.setContextMenu(contextMenu);
+            tray.on('click', () => {
+                showOverlay(); // Trigger showOverlay on tray icon click
+            });
+        }
+        mainWindow = new electron_1.BrowserWindow({
+            width: 400,
+            height: 600,
+            resizable: false,
+            autoHideMenuBar: true,
+            opacity: 0.8,
+            webPreferences: {
+                nodeIntegration: false,
+                preload: path.join(__dirname, 'preload.js'),
+                contextIsolation: true, // Isolate context for security
+            }
+        });
+        console.log(__dirname);
+        mainWindow.loadFile('./src/index.html');
+        mainWindow.hide();
+        mainWindow.webContents.openDevTools();
+        electron_1.globalShortcut.register('F7', () => {
+            showOverlay();
+        });
+    };
+    const quitApp = () => {
+        electron_1.app.quit(); // Quit the application
+    };
+    const showOverlay = () => {
+        if (mainWindow === null || mainWindow === void 0 ? void 0 : mainWindow.isDestroyed())
+            createWindow();
+        if (mainWindow === null || mainWindow === void 0 ? void 0 : mainWindow.isVisible()) {
+            mainWindow.hide();
+        }
+        else {
+            mainWindow === null || mainWindow === void 0 ? void 0 : mainWindow.show();
+        }
+    };
+    electron_1.app.whenReady().then(() => {
+        setTimeout(() => {
+            createWindow();
+        }, 10);
+    });
+    electron_1.app.on('window-all-closed', (event) => {
+        event.preventDefault();
+        //app.quit();
+    });
+    electron_1.app.on('activate', () => {
+        createWindow();
+    });
+    electron_1.ipcMain.handle('write-aime-file', (event, fileName, cardId) => {
+        let exeDirectory;
+        if (electron_1.app.isPackaged)
+            exeDirectory = path.join(electron_1.app.getAppPath(), '../../..');
+        else
+            exeDirectory = electron_1.app.getAppPath();
+        const filePath = path.join(exeDirectory, fileName);
+        try {
+            fs.writeFileSync(filePath, cardId, 'utf-8');
+        }
+        catch (error) {
+            return { error: error.message };
         }
     });
-    console.log(__dirname);
-    mainWindow.loadFile('./src/index.html');
-    mainWindow.hide();
-    mainWindow.webContents.openDevTools();
-    electron_1.globalShortcut.register('F7', () => {
-        showOverlay();
+    // TODO: Fix error "any"
+    electron_1.ipcMain.handle('read-json-file', (event, fileName) => {
+        let exeDirectory;
+        if (electron_1.app.isPackaged)
+            exeDirectory = path.join(electron_1.app.getAppPath(), '../..');
+        else
+            exeDirectory = electron_1.app.getAppPath();
+        const filePath = path.join(exeDirectory, fileName);
+        try {
+            const data = fs.readFileSync(filePath, 'utf-8');
+            return JSON.parse(data);
+        }
+        catch (error) {
+            return { error: error.message };
+        }
     });
-};
-const quitApp = () => {
-    electron_1.app.quit(); // Quit the application
-};
-const showOverlay = () => {
-    if (mainWindow === null || mainWindow === void 0 ? void 0 : mainWindow.isDestroyed())
-        createWindow();
-    if (mainWindow === null || mainWindow === void 0 ? void 0 : mainWindow.isVisible()) {
-        mainWindow.hide();
-    }
-    else {
-        mainWindow === null || mainWindow === void 0 ? void 0 : mainWindow.show();
-    }
-};
-electron_1.app.whenReady().then(() => {
-    setTimeout(() => {
-        createWindow();
-    }, 10);
-});
-electron_1.app.on('window-all-closed', (event) => {
-    event.preventDefault();
-    //app.quit();
-});
-electron_1.app.on('activate', () => {
-    createWindow();
-});
-electron_1.ipcMain.handle('write-aime-file', (event, fileName, cardId) => {
-    let exeDirectory;
-    if (electron_1.app.isPackaged)
-        exeDirectory = path.join(electron_1.app.getAppPath(), '../../..');
-    else
-        exeDirectory = electron_1.app.getAppPath();
-    const filePath = path.join(exeDirectory, fileName);
-    try {
-        fs.writeFileSync(filePath, cardId, 'utf-8');
-    }
-    catch (error) {
-        return { error: error.message };
-    }
-});
-// TODO: Fix error "any"
-electron_1.ipcMain.handle('read-json-file', (event, fileName) => {
-    let exeDirectory;
-    if (electron_1.app.isPackaged)
-        exeDirectory = path.join(electron_1.app.getAppPath(), '../..');
-    else
-        exeDirectory = electron_1.app.getAppPath();
-    const filePath = path.join(exeDirectory, fileName);
-    try {
-        const data = fs.readFileSync(filePath, 'utf-8');
-        return JSON.parse(data);
-    }
-    catch (error) {
-        return { error: error.message };
-    }
-});
-electron_1.ipcMain.handle('write-json-file', (event, fileName, data) => {
-    let exeDirectory;
-    if (electron_1.app.isPackaged)
-        exeDirectory = path.join(electron_1.app.getAppPath(), '../..');
-    else
-        exeDirectory = electron_1.app.getAppPath();
-    const filePath = path.join(exeDirectory, fileName);
-    console.log(filePath);
-    console.log(electron_1.app.isPackaged);
-    try {
-        fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
-    }
-    catch (error) {
-        return { error: error.message };
-    }
-});
-electron_1.ipcMain.handle('get-app-path', () => {
-    return electron_1.app.getAppPath();
-});
+    electron_1.ipcMain.handle('write-json-file', (event, fileName, data) => {
+        let exeDirectory;
+        if (electron_1.app.isPackaged)
+            exeDirectory = path.join(electron_1.app.getAppPath(), '../..');
+        else
+            exeDirectory = electron_1.app.getAppPath();
+        const filePath = path.join(exeDirectory, fileName);
+        console.log(filePath);
+        console.log(electron_1.app.isPackaged);
+        try {
+            fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
+        }
+        catch (error) {
+            return { error: error.message };
+        }
+    });
+    electron_1.ipcMain.handle('get-app-path', () => {
+        return electron_1.app.getAppPath();
+    });
+}
--- src/main.ts
+++ src/main.ts
@@ -7,128 +7,142 @@ let mainWindow: BrowserWindow | null;
 
 let tray : Tray | null = null;
 
-const createWindow = () => {
-    tray = new Tray(path.join(__dirname, "trayIcon.png"));
-    const contextMenu = Menu.buildFromTemplate([
-        { label: 'Show Overlay', click: showOverlay },
-        { label: 'Quit', click: quitApp}
-    ])
+const lock = app.requestSingleInstanceLock();
 
-    tray.setToolTip('My Background App');
-    tray.setContextMenu(contextMenu);
+if (!lock) {
+    app.quit();
+} else {
 
-    mainWindow = new BrowserWindow({
-        width: 400,
-        height: 600,
-        resizable: false,
-        autoHideMenuBar: true,
-        opacity: 0.8,
-        webPreferences: {
-            nodeIntegration: false,
-            preload: path.join(__dirname, 'preload.js'),
-            contextIsolation: true,                      // Isolate context for security
+    const createWindow = () => {
+        if (!tray) {
+            tray = new Tray(path.join(__dirname, "trayIcon.png"));
+            const contextMenu = Menu.buildFromTemplate([
+                { label: 'Show Overlay', click: showOverlay },
+                { label: 'Quit', click: quitApp}
+            ])
+        
+            tray.setToolTip('My Background App');
+            tray.setContextMenu(contextMenu);
+
+            tray.on('click', () => {
+                showOverlay(); // Trigger showOverlay on tray icon click
+            });
+        }
+
+
+        mainWindow = new BrowserWindow({
+            width: 400,
+            height: 600,
+            resizable: false,
+            autoHideMenuBar: true,
+            opacity: 0.8,
+            webPreferences: {
+                nodeIntegration: false,
+                preload: path.join(__dirname, 'preload.js'),
+                contextIsolation: true,                      // Isolate context for security
+            }
+        })
+
+        console.log(__dirname)
+
+        mainWindow.loadFile('./src/index.html');
+
+        mainWindow.hide();
+
+        mainWindow.webContents.openDevTools();
+
+        globalShortcut.register('F7', () => {
+            showOverlay();
+        })
+    }
+
+    const quitApp = () => {
+        app.quit(); // Quit the application
+    };
+
+    const showOverlay = () => {
+        if (mainWindow?.isDestroyed())
+            createWindow();
+
+        if (mainWindow?.isVisible()) {
+            mainWindow.hide();
+        } else {
+            mainWindow?.show();
+        }
+    }
+
+    app.whenReady().then(() => { 
+
+        setTimeout(() => { createWindow() 
+
+        }, 10) });
+
+    app.on('window-all-closed', (event: any) => {
+        event.preventDefault()
+        //app.quit();
+    })
+
+    app.on('activate', () => {
+        createWindow();
+
+    })
+
+    ipcMain.handle('write-aime-file', (event: IpcMainInvokeEvent, fileName: string, cardId: string) => {
+        let exeDirectory;
+
+        if (app.isPackaged)
+            exeDirectory = path.join(app.getAppPath(), '../../..')
+        else
+            exeDirectory = app.getAppPath();
+
+        const filePath = path.join(exeDirectory, fileName);
+
+        try {
+            fs.writeFileSync(filePath, cardId, 'utf-8');
+        } catch (error: any) {
+            return { error: error.message }
         }
     })
 
-    console.log(__dirname)
+    // TODO: Fix error "any"
+    ipcMain.handle('read-json-file', (event: IpcMainInvokeEvent, fileName: string) : CardData | any => {
+        let exeDirectory;
 
-    mainWindow.loadFile('./src/index.html');
+        if (app.isPackaged)
+            exeDirectory = path.join(app.getAppPath(), '../..');
+        else
+            exeDirectory = app.getAppPath();
 
-    mainWindow.hide();
+        const filePath = path.join(exeDirectory, fileName);
 
-    mainWindow.webContents.openDevTools();
+        try {
+            const data = fs.readFileSync(filePath, 'utf-8');
+            return JSON.parse(data) as CardData;
+        } catch (error: any) {
+            return { error: error.message }
+        }
+    })
 
-    globalShortcut.register('F7', () => {
-        showOverlay();
+    ipcMain.handle('write-json-file', (event: IpcMainInvokeEvent, fileName: string, data: CardData[]) => {
+        let exeDirectory;
+
+        if (app.isPackaged)
+            exeDirectory = path.join(app.getAppPath(), '../..');
+        else
+            exeDirectory = app.getAppPath();
+        const filePath = path.join(exeDirectory, fileName);
+
+        console.log(filePath);
+        console.log(app.isPackaged);
+
+        try {
+            fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
+        } catch (error: any) {
+            return { error: error.message }
+        }
+    })
+
+    ipcMain.handle('get-app-path', () => {
+        return app.getAppPath();
     })
 }
-
-const quitApp = () => {
-    app.quit(); // Quit the application
-};
-
-const showOverlay = () => {
-    if (mainWindow?.isDestroyed())
-        createWindow();
-
-    if (mainWindow?.isVisible()) {
-        mainWindow.hide();
-    } else {
-        mainWindow?.show();
-    }
-}
-
-app.whenReady().then(() => { 
-
-    setTimeout(() => { createWindow() 
-
-    }, 10) });
-
-app.on('window-all-closed', (event: any) => {
-    event.preventDefault()
-    //app.quit();
-})
-
-app.on('activate', () => {
-    createWindow();
-    
-})
-
-ipcMain.handle('write-aime-file', (event: IpcMainInvokeEvent, fileName: string, cardId: string) => {
-    let exeDirectory;
-
-    if (app.isPackaged)
-        exeDirectory = path.join(app.getAppPath(), '../../..')
-    else
-        exeDirectory = app.getAppPath();
-
-    const filePath = path.join(exeDirectory, fileName);
-
-    try {
-        fs.writeFileSync(filePath, cardId, 'utf-8');
-    } catch (error: any) {
-        return { error: error.message }
-    }
-})
-
-// TODO: Fix error "any"
-ipcMain.handle('read-json-file', (event: IpcMainInvokeEvent, fileName: string) : CardData | any => {
-    let exeDirectory;
-    
-    if (app.isPackaged)
-        exeDirectory = path.join(app.getAppPath(), '../..');
-    else
-        exeDirectory = app.getAppPath();
-
-    const filePath = path.join(exeDirectory, fileName);
-
-    try {
-        const data = fs.readFileSync(filePath, 'utf-8');
-        return JSON.parse(data) as CardData;
-    } catch (error: any) {
-        return { error: error.message }
-    }
-})
-
-ipcMain.handle('write-json-file', (event: IpcMainInvokeEvent, fileName: string, data: CardData[]) => {
-    let exeDirectory;
-    
-    if (app.isPackaged)
-        exeDirectory = path.join(app.getAppPath(), '../..');
-    else
-        exeDirectory = app.getAppPath();
-    const filePath = path.join(exeDirectory, fileName);
-
-    console.log(filePath);
-    console.log(app.isPackaged);
-
-    try {
-        fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
-    } catch (error: any) {
-        return { error: error.message }
-    }
-})
-
-ipcMain.handle('get-app-path', () => {
-    return app.getAppPath();
-})
